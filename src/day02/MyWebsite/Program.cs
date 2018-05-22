5330de77857e3d4c69ea5ce1b1b7098d8ba667c8
zhaoming <zhaoming@mallcoo.cn>
2018-05-22 11:49:07 +0800

    day02

---
+++ src/day02/MyWebsite/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyWebsite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Output("Application - Start");
+            var webHost = BuildWebHost(args);
+            Output("Run WebHost");
+            webHost.Run();
+            Output("Application - End");
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            Output("Create WebHost Builder");
+            var webHostBuilder = WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    Output("webHostBuilder.ConfigureServices - Called");
+                })
+                .Configure(app =>
+                {
+                    Output("webHostBuilder.Configure - Called");
+                })
+                .UseStartup<Startup>();
+
+            Output("Build WebHost");
+            var webHost = webHostBuilder.Build();
+
+            return webHost;
+        }
+
+        public static void Output(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}] {message}");
+        }
+    }
+}
---
+++ src/day02/MyWebsite/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyWebsite
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            Program.Output("Startup Constructor - Called");
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Program.Output("Startup.ConfigureServices - Called");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IApplicationLifetime appLifetime)
+        {
+            appLifetime.ApplicationStarted.Register(() =>
+            {
+                Program.Output("ApplicationLifetime - Started");
+            });
+
+            appLifetime.ApplicationStopping.Register(() =>
+            {
+                Program.Output("ApplicationLifetime - Stopping");
+            });
+
+            appLifetime.ApplicationStopped.Register(() =>
+            {
+                Thread.Sleep(5 * 1000);
+                Program.Output("ApplicationLifetime - Stopped");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+            // For trigger stop WebHost
+            var thread = new Thread(new ThreadStart(() =>
+            {
+                Thread.Sleep(5 * 1000);
+                Program.Output("Trigger stop WebHost");
+                appLifetime.StopApplication();
+            }));
+            thread.Start();
+
+            Program.Output("Startup.Configure - Called");
+        }
+    }
+}
